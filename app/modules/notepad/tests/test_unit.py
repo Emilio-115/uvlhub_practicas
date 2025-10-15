29dd9bd97de702ac3b79c419ad0e4c4ba13ba44e
Emilio-115 <emiliovc2016@gmail.com>
2025-10-15 14:09:19 +0200

    feat: Add unit test for notepad

--- app/modules/notepad/tests/test_unit.py
+++ app/modules/notepad/tests/test_unit.py
@@ -1,4 +1,8 @@
 import pytest
+from app import db
+from app.modules.notepad.models import Notepad
+from app.modules.auth.models import User
+from app.modules.conftest import login, logout
 
 
 @pytest.fixture(scope='module')
@@ -9,16 +13,31 @@ def test_client(test_client):
     with test_client.application.app_context():
         # Add HERE new elements to the database that you want to exist in the test context.
         # DO NOT FORGET to use db.session.add(<element>) and db.session.commit() to save the data.
-        pass
+        user_test = User(email="user@example.com", password="test1234")
+        db.session.add(user_test)
+        db.session.commit()
+
+        notepad = Notepad(user_id=user_test.id, title="Test title",body="This is the testing body of the notepad")
+        db.session.add(notepad)
+        db.session.commit()
 
     yield test_client
 
 
-def test_sample_assertion(test_client):
+def test_get_notepad_page(test_client):
     """
     Sample test to verify that the test framework and environment are working correctly.
     It does not communicate with the Flask application; it only performs a simple assertion to
     confirm that the tests in this module can be executed.
     """
-    greeting = "Hello, World!"
-    assert greeting == "Hello, World!", "The greeting does not coincide with 'Hello, World!'"
+
+    login_response = login(test_client, "user@example.com", "test1234")
+    assert login_response.status_code == 200, "Login was unsuccessful."
+
+    response = test_client.get("/notepad")
+    assert response.status_code == 200, "The profile editing page could not be accessed."
+    assert b"Delete" in response.data, "The expected content is not present on the page"
+
+    logout(test_client)
+
+
